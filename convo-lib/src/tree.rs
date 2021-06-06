aae5157a02ea391560b21f842879840107af2fc5
Spencer Imbleau <spencer@imbleau.com>
2021-06-06 13:04:15 -0400

    Changed tree root and current to be options

--- convo-lib/src/tree.rs
+++ convo-lib/src/tree.rs
@@ -1,48 +1,60 @@
 use std::collections::HashMap;
 
-use crate::{link::Link, node::Node};
+use crate::node::Node;
 
 #[derive(Debug)]
 pub struct CTree {
     nodes: HashMap<String, Node>,
-    root: String,
-    current: String,
+    root: Option<String>,
+    current: Option<String>,
+}
+
+impl Default for CTree {
+    // Provides a default empty CTree
+    fn default() -> Self {
+        CTree {
+            nodes: HashMap::<String, Node>::new(),
+            root: None,
+            current: None,
+        }
+    }
 }
 
 impl CTree {
-    // Example tree for testing
-    pub fn example_tree() -> CTree {
-        // Build nodes
-        let mut node1 = Node::new("root".to_string(), vec![], "How are you?".to_string());
-        let node2 = Node::new("n2".to_string(), vec![], "Don't be late!".to_string());
-
-        // Link node1 -> node 2
-        node1.links_mut().insert(
-            0,
-            Link::from(node2.key().clone(), "In a hurry!".to_string()),
-        );
-
-        // Declare nodes
-        let mut nodes = HashMap::<String, Node>::new();
-        nodes.insert(node1.key().clone(), node1);
-        nodes.insert(node2.key().clone(), node2);
-
-        CTree {
-            nodes,
-            root: "root".to_string(),
-            current: "root".to_string(),
-        }
-    }
-
     // Construct a dialogue tree
     pub fn from(_source: &str, _root: &str) -> Result<CTree, &'static str> {
         todo!("Not yet implemented");
     }
 
+    // Immutable access to root
+    pub fn root(&self) -> Option<&String> {
+        self.root.as_ref()
+    }
+
+    // Set the root
+    pub fn set_root(&mut self, node: &Node) -> Result<&CTree, &'static str> {
+        // Take ownership if necessary
+        if self.root.is_some() {
+            self.root.take().unwrap();
+        }
+
+        self.root = Some(node.key().clone());
+        Ok(self)
+    }
+
     // Reset the current node to root
-    pub fn reset(&mut self) -> &CTree {
-        self.current = self.root.clone();
-        self
+    pub fn reset(&mut self) -> Result<&CTree, &'static str> {
+        if self.root.is_none() {
+            return Err("Root is none");
+        }
+
+        // Take ownership if necessary
+        if self.current.is_some() {
+            self.current.take().unwrap();
+        }
+
+        self.current = Some(self.root().unwrap().clone());
+        Ok(self)
     }
 
     // Immutable access to node
--- convo-lib/tests/integration_test.rs
+++ convo-lib/tests/integration_test.rs
@@ -1,11 +1,33 @@
-use convo_lib::tree::CTree;
+use convo_lib::{link::Link, node::Node, tree::CTree};
 
 extern crate convo_lib;
 
 #[test]
 // Basic print out of all nodes and links - Not a great test
 fn test_print() {
-    let tree = CTree::example_tree();
+    let mut tree = CTree::default();
+
+    // Build some nodes
+    let mut node1 = Node::new("root".to_string(), vec![], "How are you?".to_string());
+    let node2 = Node::new("end".to_string(), vec![], "Don't be late!".to_string());
+
+    // Set node1 as root
+    tree.set_root(&node1).unwrap();
+
+    // Link node1 -> node 2
+    node1.links_mut().insert(
+        0,
+        Link::from(node2.key().clone(), "Sorry, I'm in a hurry!".to_string()),
+    );
+
+    // Populate the tree
+    tree.nodes_mut().insert(node1.key().clone(), node1);
+    tree.nodes_mut().insert(node2.key().clone(), node2);
+
+    // Print root node:
+    println!("Node root: [{}]", tree.root().unwrap());
+
+    // Print them all out
     for (_, node) in tree.nodes() {
         println!("Node [{}]: '{}'", node.key(), node.description());
         for link in node.links() {
