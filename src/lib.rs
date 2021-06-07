4386346e4a9ead16813a28a968b5945fe8a40069
Spencer Imbleau <spencer@imbleau.com>
2021-06-07 18:10:58 -0400

    Enabled doc requirements

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 // Enforce stricter documentation requirements
-/*
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
 #![warn(rustdoc::missing_doc_code_examples)]
 #![warn(rustdoc::invalid_codeblock_attributes)]
 #![warn(rustdoc::broken_intra_doc_links)]
-*/
 
 //! A modern dialogue executor and tree parser using Extended Backus-Naur Form.
 extern crate nom;
