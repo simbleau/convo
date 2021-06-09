fac21a6b0091986ab5f73362dc7d29704efc197a
Spencer Imbleau <spencer@imbleau.com>
2021-06-08 20:08:48 -0400

    Added partial parsing example (Not working)

---
+++ examples/parsing.rs
@@ -0,0 +1,13 @@
+use std::{env, path::Path};
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    assert!(args.len() == 2, "You must specify a file.");
+
+    let path = Path::new(&args[1]);
+    println!("Selected file: {}", path.to_str().unwrap());
+
+    println!("Parsing...");
+    //let ctree = convo::parser::parse(path).unwrap();
+    println!("Completed.\n\n{:?}", ctree);
+}
